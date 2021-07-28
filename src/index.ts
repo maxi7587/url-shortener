7b90e31b2065d0044ecddeeca6d2c4d8594be92d
Maxi Cruz <maximiliano.cruz@mercadolibre.com>
2021-07-28 02:44:37 -0300

    dockerize, add dotenv and basic folder structure

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello world!')
+import express, {Express, Request, Response} from 'express';
+import compression from 'compression';
+import dotenv from 'dotenv';
+
+dotenv.config({path: 'src/config/.env'});
+
+// Constants
+const PORT: number = parseInt(process.env.PORT, 10);
+const HOST: string = process.env.HOST;
+
+// App
+const app: Express = express();
+app.use(compression());
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
